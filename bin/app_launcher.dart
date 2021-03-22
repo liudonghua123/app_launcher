e675356bd79da5a37c6b074d5aa1d17ddf98bc5a
liudonghua123 <liudonghua123@gmail.com>
2021-03-22 14:06:14 +0800

    update code

--- bin/app_launcher.dart
+++ bin/app_launcher.dart
@@ -1,11 +1,23 @@
 import 'dart:io';
+import 'package:path/path.dart' as path;
 import 'package:logger/logger.dart';
 import 'package:yaml/yaml.dart';
 
-// import 'package:app_launcher/app_launcher.dart' as app_launcher;
+import 'package:logger/src/outputs/file_output.dart';
 
 void main(List<String> arguments) {
-  var logger = Logger();
+  var workingDirectory = path.dirname(Platform.executable);
+  var logFile = File(path.join(workingDirectory, 'log.txt'));
+  var logger = Logger(
+    filter: ProductionFilter(),
+    output: MultiOutput(
+      [
+        ConsoleOutput(),
+        FileOutput(file: logFile),
+      ],
+    ),
+  );
+  logger.i('workingDirectory: $workingDirectory');
   var configFile = File('config.yaml');
   if (!configFile.existsSync()) {
     logger.e('config.yaml file not found!');
@@ -13,10 +25,23 @@ void main(List<String> arguments) {
   }
   var config = loadYaml(configFile.readAsStringSync());
   logger.i('config: $config');
+  var arguments = (config['arguments'] as YamlList).value.map((e) {
+    var argument = e.toString();
+    // handle --user-data-dir argument separated
+    var userDataDirArgName = '--user-data-dir';
+    if (argument.startsWith(userDataDirArgName)) {
+      var userDataDirArgs = argument.split('=');
+      return '$userDataDirArgName=${path.join(workingDirectory, userDataDirArgs[1])}';
+    }
+    return argument;
+  }).toList();
+  logger.i('arguments: $arguments');
   var result = Process.runSync(
     config['app'],
-    (config['arguments'] as YamlList).value.map((e) => e.toString()).toList(),
+    arguments,
     runInShell: false,
+    workingDirectory: workingDirectory,
   );
-  logger.i('result: ${result.exitCode}, ${result.stdout}, ${result.stderr}');
+  logger.i(
+      'result: exitCode: ${result.exitCode}, stdout: ${result.stdout}, stderr: ${result.stderr}');
 }
